d1811cb9ebd1664e9c38ea472bbc540e14979b22
GutoQuirino <tavinho_vh@hotmail.com>
2022-06-29 12:51:07 -0300

    add comentarios

--- script.js
+++ script.js
@@ -4,17 +4,16 @@ const c = (el)=>document.querySelector(el);
 const cs = (el)=>document.querySelectorAll(el);
 
 pizzaJson.map((item,index)=>{
-    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);//mapeia os itens e clona a div
 
-    pizzaItem.setAttribute('data-key',index);
+    pizzaItem.setAttribute('data-key',index);//atribui a propriedade atraves do indice
     pizzaItem.querySelector('.pizza-item--img img').src = item.img; 
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('a').addEventListener('click',(e)=>{
-        e.preventDefault();
-        //anula a funçao padrao do click
-
+        e.preventDefault(); //anula a funçao padrao do click
+        
         modalQt = 1;
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
 
@@ -24,8 +23,7 @@ pizzaJson.map((item,index)=>{
         c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
         
         c('.pizzaInfo--size.selected').classList.remove('selected');
-        cs('.pizzaInfo--size').forEach((size,sizeIndex)=>{
-            //forEach preenche todos os itens que contem na classe
+        cs('.pizzaInfo--size').forEach((size,sizeIndex)=>{ //forEach preenche todos os itens que contem na classe
             if(sizeIndex ==2){
                 size.classList.add('selected');
             }
@@ -34,10 +32,9 @@ pizzaJson.map((item,index)=>{
 
         c('.pizzaInfo--qt').innerHTML = modalQt;
 
-        c('.pizzaWindowArea').style.display = 'flex';
+        c('.pizzaWindowArea').style.display = 'flex'; //abre o modal
         c('.pizzaWindowArea').style.opacity = '0';
-        //cria delay na animaçao de abrir o modal
-        setTimeout(()=>{
+        setTimeout(()=>{ //cria delay na animaçao de abrir o modal
             c('.pizzaWindowArea').style.opacity = '1';
         },200);
 
