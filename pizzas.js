f633d2f24791fb09061eb5b05f81ce07d648f5eb
GutoQuirino <tavinho_vh@hotmail.com>
2022-07-20 10:38:38 -0300

    alteraçao de dados do json

--- pizzas.js
+++ pizzas.js
@@ -1,9 +1,9 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'images/pizza.png', price:27.99, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Calabresa', img:'images/pizza2.png', price:28.99, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:32.50, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Americana', img:'images/pizza4.png', price:31.90, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Sorvete', img:'images/pizza5.png', price:37.50, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:42.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Chocolate', img:'images/pizza7.png', price:38.90, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+    {id:1, name:'Peperone', img:'images/pizza.png', price:27.99, sizes:['100g', '530g', '860g'], description:'Molho de tomate, mussarela, oregano e azeitonas pretas'},
+    {id:2, name:'Marguerita', img:'images/pizza2.png', price:28.99, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela e mangericão'},
+    {id:3, name:'Canadense', img:'images/pizza3.png', price:32.50, sizes:['320g', '530g', '860g'], description:'Lombo defumado, creme de leite, pimentão, tomate e azeitonas pretas'},
+    {id:4, name:'Brócolis com Requeijao', img:'images/pizza4.png', price:31.90, sizes:['320g', '530g', '860g'], description:'Molho de tomate, requeijao, brócolis, tomate e azeitonas pretas'},
+    {id:5, name:'Caipira', img:'images/pizza5.png', price:37.50, sizes:['320g', '530g', '860g'], description:'Molho de tomate, milho, tomate, abobrimha, azeitonas pretas coberta com requeijao cremoso'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:42.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, presunto, milho, azeitonas e coberta com requeijão'},
+    {id:7, name:'Confete', img:'images/pizza7.png', price:38.90, sizes:['320g', '530g', '860g'], description:'Pizza de chocolate ao leite coberta com confete e leite condensado'}
 ];
